009616270e98e02eb0533b62c8db1c81921430d1
Osa <osaojoosagie@gmail.com>
2024-05-30 22:39:00 +0100

    Updated and re-structured

--- scr/RiskCalc.py
+++ scr/RiskCalc.py
@@ -3,9 +3,12 @@ Author: OsaO
 Date Created: 19/05/2024
 
 Last Updated By: OsaO
-Last Updated Date: 19/05/2024
+Last Updated Date: 30/05/2024
 
-Summary: 
+Summary:
+Risk simulator showing potential equity curve for a trading strategy with defined parameters.
+    - Parameters: Starting Balance, Strategy Win Rate, Risk Percentage, Average trade Risk-to-Reward,
+                  Number of trades taken, Number of simulations wanted 
 """
 from random import choices
 import matplotlib.pyplot as plt
@@ -28,44 +31,30 @@ class RiskSimulation:
         # An array storing the balace tracking arrays for each simulation performed
         self.outcomes = []
 
-        self.main()
-
-    def main(self):
-        """
-        Uses the imported class variables to plot equity curve(s)
-        """
-
+    def runSimulation(self):
+        # Runs the Risk Simulation
+        # All the required inputs should have already been provided when class decleared
+        
         # Threading used to run number of simulations simultaneously
         threads = []
 
         for i in range(self.noSimulations):
-            t = threading.Thread(target=self.FixedRisk(self.startBalance))
+            t = threading.Thread(target=self.FixedRiskSim(self.startBalance))
             threads.append(t)
 
         # Starts all threads
         for x in threads:
             x.start()
-
         # Waits for all threads to finish
         for x in threads:
             x.join()
         
-        # All threads should be completed by this point
-
-        # Gets list of xValues used to plot graph
-        # xValues = number of trades, so generates list starting from 0 to number of trades with a step of 1
-        xValues = np.arange(0, self.noTrades+1, 1).tolist()
-
-        # Plots each of the outcomes
-        for outcome in self.outcomes:
-            plt.plot(xValues, outcome)
+        ### All threads should be completed by this point ###      
     
-        # Sets chart environemnt
-        plt.xlabel('Number of Trades')
-        plt.ylabel('Account Balance')
-        plt.grid('on')
-        plt.show()
-
+    def returnOutcome(self):
+        # Returns the outcome of the runs generated
+        return self.outcomes
+    
     def FixedRiskSim(self, accBalance):
         """
         Fixed Risk Simulation
@@ -95,9 +84,27 @@ class RiskSimulation:
             # Appends new balance to tracking list  
             balanceTracking.append(accBalance)
         (self.outcomes).append(balanceTracking)
+    
+    def plot(self):
+        """
+        Not used by the app - for testing / debugging within script only
+        """
+        # Gets list of xValues used to plot graph
+        # xValues = number of trades, so generates list starting from 0 to number of trades with a step of 1
+        xValues = np.arange(0, self.noTrades+1, 1).tolist()
 
+        # Plots each of the outcomes
+        for outcome in self.outcomes:
+            plt.plot(xValues, outcome)
+    
+        # Sets chart environemnt
+        plt.xlabel('Number of Trades')
+        plt.ylabel('Account Balance')
+        plt.grid('on')
+        plt.show()
 
 if __name__ == "__main__":
+    # Debugging / Testing
     balance = 100000
     winrate = 0.60
     riskPercentage = 1
